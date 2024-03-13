15864cdecd225d1aa7bd8bca3c70af19da1498d8
Viktor Reuterhäll <viktor.reuterhall@skola.sala.se>
2024-03-13 12:48:32 +0100

    Add project files.

---
+++ Gissa_talet/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Gissa_Talet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int talet = random.Next(1, 21);
+            int gissning = 0;
+            int gissningar = 0;
+            Console.WriteLine("Gissa ett tal mellan 1-20");
+            while (gissning != talet && gissningar <= 2)
+            {
+                gissning = Convert.ToInt32(Console.ReadLine());
+                if (gissning == talet)
+                {
+                    Console.WriteLine("Bra jobbat! Talet var " + talet);
+                    Console.ReadLine();
+                }
+                else if (gissning < talet)
+                {
+                    Console.WriteLine("För lågt!");
+
+                }
+
+                else if (gissning > talet)
+                {
+                    Console.WriteLine("För högt!");
+
+                }
+                gissningar++;
+
+               
+            }
+            Console.WriteLine("Du gissade fel!");
+        }
+    }
+}
